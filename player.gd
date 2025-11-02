b7553714724665639d64976af6ea37282ca46a66
lkopacka <lkopacka@madfingergames.com>
2025-11-02 16:26:04 +0100

    commit fixes

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var SPEED = 300.0
+@export var player_move_speed = 300.0
 @export var max_health = 100
 var current_health = max_health
 
@@ -33,14 +33,5 @@ func _physics_process(_delta):
 	if vertical:
 		velocity.y = vertical * player_move_speed
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, player_move_speed)
 	move_and_slide()
-	health_system()
-#Health System
-func take_damage(damage):
-	player_health = player_health - damage
-	print(player_health)
-func health_system():
-	
-	if (player_health <= 0):
-		get_tree().reload_current_scene()
