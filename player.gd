b83162e5e8cbe35ea52c20b861a532940163dc72
lkopacka <lkopacka@madfingergames.com>
2025-11-02 16:38:52 +0100

    fix get damage

--- player.gd
+++ player.gd
@@ -2,11 +2,15 @@ extends CharacterBody2D
 
 @export var player_move_speed = 300.0
 @export var max_health = 100
+@export var damage_cooldown = 1.0
+
 var current_health = max_health
+var damage_timer = 0.0
 
 func _ready():
 	current_health = max_health
 	$Area2D.body_entered.connect(_on_body_entered)
+
 
 func _on_body_entered(body):
 	if body.is_in_group("enemy"):
@@ -35,3 +39,12 @@ func _physics_process(_delta):
 	else:
 		velocity.y = move_toward(velocity.y, 0, player_move_speed)
 	move_and_slide()
+	
+	damage_timer -= _delta
+	if damage_timer <= 0:
+		var overlapping = $Area2D.get_overlapping_bodies()
+		for body in overlapping:
+			if body.is_in_group("enemy"):
+				take_damage(10)
+				damage_timer = damage_cooldown
+				break  # Only take damage from one enemy per cooldown
