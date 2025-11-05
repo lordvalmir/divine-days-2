038004844af569171f1ce093cd22676b88c25f18
djunk <dawe.junk@gmail.com>
2025-11-05 21:05:19 +0100

    chnages

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @export var enemy_move_speed: float = 130
+@export var max_health = 100
+var current_health = max_health
  
 @onready var player = get_tree().get_first_node_in_group("Player")
 @warning_ignore("unused_parameter")
@@ -12,3 +14,12 @@ func _physics_process(_delta: float) -> void:
 		var direction = position.direction_to(player.global_position).normalized()
 		velocity = direction * enemy_move_speed
 		move_and_slide()
+		
+func take_damage(amount):
+	current_health -= amount
+	
+	if current_health <= 0:
+		die()
+
+func die():
+	queue_free()
